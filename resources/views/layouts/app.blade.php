f53f77e6f7bc8c6bbb113094e4e697141462c79b
Ramón Martos Oliver <soughtsingularity@gmail.com>
2025-02-19 19:52:39 +0100

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Mi Aplicación')</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-100 dark:bg-gray-800">
+
+    @yield('content')
+
+</body>
+</html>
+
